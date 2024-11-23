314ee35584c0fbe20f5c294f11aa348bb78f152a
Fikiremariyam <dgi123baba@gmail.com>
2024-11-23 13:44:14 +0200

    question 1 and 2

--- Week two assignments/question1.go
+++ Week two assignments/question1.go
@@ -1,12 +1,34 @@
+/* qustion1
+1. Write a function ResizeSlice that takes a slice of integers and a
+target length as arguments. If the slice is smaller than the target
+length, append zeros until it reaches the target length. If it's larger,
+truncate the slice. Ensure that the function handles the edge cases
+correctly (e.g., when the target length is zero or negative).
+
+*/
+
 package main
 
 import (
 	"fmt"
+	"slices"
 )
 
-func main() {
-	fmt.Printf("hello world")
-
+func ResizeSlice(numbers []int, target int) []int {
+	if len(numbers) == 0 {
+		return numbers
+	}
+	if len(numbers) < target {
+		for i := 0; i < target-len(numbers); i++ {
+			numbers = slices.Insert(numbers, len(numbers), 0)
+		}
+	}
+	return numbers[:target]
 }
 
-//new tasks
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6, 8, 8, 9}
+	final := ResizeSlice(slice, 4)
+
+	fmt.Println(final)
+}
---
+++ Week two assignments/question2.go
@@ -0,0 +1,42 @@
+/*
+	 qestion 2
+
+	Implement a function WordFrequency that takes a slice of strings
+
+representing words and returns a map with each unique word as a
+key and its count as the value. Ensure the function is case-insensitive
+and ignores punctuation.
+*/
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func Counter[T comparable](elements []T) map[T]int {
+	counts := make(map[T]int)
+	for _, element := range elements {
+		counts[element]++
+	}
+	return counts
+}
+
+func WordFrequency(words []string) map[string]int {
+
+	count := make(map[string]int)
+	for _, element := range words {
+		count[strings.ToLower(element)]++
+	}
+
+	return count
+
+}
+
+func main() {
+
+	string := "my name name Name MY "
+	slice := strings.Split(string, " ")
+
+	fmt.Println(WordFrequency(slice))
+}
